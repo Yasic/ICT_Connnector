259a9e6bc649602736074302cc1b7c59d4b56795
Yasic <2207793765@qq.com>
2017-01-21 13:24:07 +0800

    Update Version1.3

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ICT_ConnnectorV1.2
+# ICT_ConnnectorV1.3
 需要自取，无可奉告
--- asset/ict_connector.js
+++ asset/ict_connector.js
@@ -72,76 +72,80 @@ function do_post_data() {
     return post_data("http://159.226.39.22/cgi-bin/do_login", "post", data);
 }
 
+function present_error_info(error){
+    document.getElementById("error_info").innerHTML = error;
+}
+
 function show_error_info() {
     switch (post_result) {
         case "user_tab_error":
-            alert("认证程序未启动");
+            present_error_info("认证程序未启动");
             break;
 
         case "username_error":
-            alert("用户名错误");
+            present_error_info("用户名错误");
             document.form1.uname.focus();
             break;
 
         case "non_auth_error":
-            alert("您无须认证，可直接上网");
+            present_error_info("您无须认证，可直接上网");
             break;
 
         case "password_error":
-            alert("密码错误");
+            present_error_info("密码错误");
             document.form1.pass.focus();
             break;
 
         case "status_error":
-            alert("用户已欠费，请尽快充值。");
+            present_error_info("用户已欠费，请尽快充值。");
             break;
 
         case "available_error":
-            alert("用户已禁用");
+            present_error_info("用户已禁用");
             break;
 
         case "ip_exist_error":
-            alert("您的IP尚未下线，请等待2分钟再试。");
+            present_error_info("您的IP尚未下线，请等待2分钟再试。");
             break;
 
         case "usernum_error":
-            alert("用户数已达上限");
+            present_error_info("用户数已达上限");
             break;
 
         case "online_num_error":
-            alert("该帐号的登录人数已超过限额\n如果怀疑帐号被盗用，请联系管理员。");
+            present_error_info("该帐号的登录人数已超过限额\n如果怀疑帐号被盗用，请联系管理员。");
             break;
 
         case "mode_error":
-            alert("系统已禁止WEB方式登录，请使用客户端");
+            present_error_info("系统已禁止WEB方式登录，请使用客户端");
             break;
 
         case "time_policy_error":
-            alert("当前时段不允许连接");
+            present_error_info("当前时段不允许连接");
             break;
 
         case "flux_error":
-            alert("您的流量已超支");
+            present_error_info("您的流量已超支");
             break;
 
         case "minutes_error":
-            alert("您的时长已超支");
+            present_error_info("您的时长已超支");
             break;
 
         case "ip_error":
-            alert("您的IP地址不合法");
+            present_error_info("您的IP地址不合法");
             break;
 
         case "mac_error":
-            alert("您的MAC地址不合法");
+            present_error_info("您的MAC地址不合法");
             break;
 
         case "sync_error":
-            alert("您的资料已修改，正在等待同步，请2分钟后再试。");
+            present_error_info("您的资料已修改，正在等待同步，请2分钟后再试。");
             break;
 
         default:
-            alert("找不到认证服务器");
+            present_error_info("找不到认证服务器");
             break;
     }
 }
@@ -166,13 +170,15 @@ function do_login() {
     document.getElementById("running_outside_div").style.display = "block";
 
     if (is_online) {
+        present_error_info("")
     }
     else {
         post_result = do_post_data();
         temp_reg = /^[\d]+$/;
         if (temp_reg.test(post_result)) { //login successful
             console.log("Login successful!");
-        } else { //show error info
+            present_error_info("");
+        } else {
             show_error_info();
             document.getElementById("login_button").style.display = "block";
             document.getElementById("username_div").style.display = "block";
