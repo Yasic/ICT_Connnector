78278e24560046ac76d1df482a060e41e22ba1a3
yasic <2207793765@qq.com>
2017-03-12 19:32:14 +0800

    update

--- asset/ict_connector.js
+++ asset/ict_connector.js
@@ -1,14 +1,9 @@
 var is_online = false;
 var flag_img = new Image();//img for checking network
-var xml_http_request = (window.XMLHttpRequest)? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
 var user_name;//username
 var password;//password
 var temp_pass;
 var final_pass;
-var drop; //useless element
-var data;
-var post_result;
-var temp_reg;
 
 
 window.onload = function(){
@@ -49,37 +44,13 @@ function check_input() {
     return 0;
 }
 
-function post_data(theAction, theMethod, theData) {
-    switch(theMethod) {
-        case "post":
-            xml_http_request.open("POST", theAction, false);
-            xml_http_request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xml_http_request.send(theData);
-            break;
-        default:
-            return "";
-    }
-    return xml_http_request.responseText;
-}
-
 function getRealPass() {
-    password = document.getElementById("password").value;
+    password = document.getElementById("user_password").value;
     temp_pass = hex_md5(password);
-    /*temp_pass = "12345678123456781234567812345678";*/
+    /*temp_pass = "pppaaassswwwooorrrd" + password + "password";*/
     final_pass = temp_pass.substr(8, 16);
     document.getElementById("password").value = final_pass;
 }
-
-/*function do_post_data() {
-    //密码md5加密传送
-    //temp_pass = hex_md5(password);
-    final_pass = temp_pass.substr(8, 16);
-    drop = 1;
-    data = "username=" + user_name + "&password=" + final_pass + "&drop=" + drop + "&type=1&n=100";
-
-    //return "123";
-    return post_data("http://159.226.39.22/cgi-bin/do_login", "post", data);
-}*/
 
 function present_error_info(error){
     document.getElementById("error_info").innerHTML = error;
@@ -126,92 +97,5 @@ function do_login() {
                 document.getElementById("main_form").submit();
             }
         }, 1000);
-        /*temp_reg = /^[\d]+$/;
-        if (temp_reg.test(post_result)) { //login successful
-            console.log("Login successful!");
-            present_error_info("");
-        } else {
-            show_error_info(post_result);
-            document.getElementById("login_div").style.display = "block";
-            document.getElementById("username_div").style.display = "block";
-            document.getElementById("password_div").style.display = "block";
-            document.getElementById("login_div").value = "Run";
-            return;
-        }*/
-    }
-}
-
-
-function show_error_info(post_result) {
-    switch (post_result) {
-        case "user_tab_error":
-            present_error_info("认证程序未启动");
-            break;
-
-        case "username_error":
-            present_error_info("用户名错误");
-            document.form1.uname.focus();
-            break;
-
-        case "non_auth_error":
-            present_error_info("您无须认证，可直接上网");
-            break;
-
-        case "password_error":
-            present_error_info("密码错误");
-            document.form1.pass.focus();
-            break;
-
-        case "status_error":
-            present_error_info("用户已欠费，请尽快充值。");
-            break;
-
-        case "available_error":
-            present_error_info("用户已禁用");
-            break;
-
-        case "ip_exist_error":
-            present_error_info("您的IP尚未下线，请等待2分钟再试。");
-            break;
-
-        case "usernum_error":
-            present_error_info("用户数已达上限");
-            break;
-
-        case "online_num_error":
-            present_error_info("该帐号的登录人数已超过限额\n如果怀疑帐号被盗用，请联系管理员。");
-            break;
-
-        case "mode_error":
-            present_error_info("系统已禁止WEB方式登录，请使用客户端");
-            break;
-
-        case "time_policy_error":
-            present_error_info("当前时段不允许连接");
-            break;
-
-        case "flux_error":
-            present_error_info("您的流量已超支");
-            break;
-
-        case "minutes_error":
-            present_error_info("您的时长已超支");
-            break;
-
-        case "ip_error":
-            present_error_info("您的IP地址不合法");
-            break;
-
-        case "mac_error":
-            present_error_info("您的MAC地址不合法");
-            break;
-
-        case "sync_error":
-            present_error_info("您的资料已修改，正在等待同步，请2分钟后再试。");
-            break;
-
-        default:
-            present_error_info("找不到认证服务器");
-            break;
     }
 }
