42ea2dc6ca25fdac25448e667bea9d6fb7701119
yasic <2207793765@qq.com>
2017-03-13 10:18:28 +0800

    update UI

--- asset/ict_connector.js
+++ asset/ict_connector.js
@@ -6,23 +6,21 @@ var temp_pass;
 var final_pass;
 
 
-window.onload = function(){
+window.onload = function () {
     check_internet_status();
 };
 
-function check_internet_status(){
+function check_internet_status() {
     flag_img.onload = function () {
         is_online = true;
-        document.getElementById("check_internet_img").setAttribute("src", flag_img.src);
-        document.getElementById("warning").style.display = "none"
-        document.getElementById("presenter").style.display = "block"
+        document.getElementById("online").style.display = "block";
+        document.getElementById("offline").style.display = "none";
     };
-    flag_img.onerror = function (){
+    flag_img.onerror = function () {
         is_online = false;
-        console.log("Offline! Ready for login...")
-        document.getElementById("check_internet_img").setAttribute("src", "asset/image/blue_cross_128px.png");
-        document.getElementById("warning").style.display = "block";
-        document.getElementById("presenter").style.display = "none"
+        console.log("Offline! Ready for login...");
+        document.getElementById("online").style.display = "none";
+        document.getElementById("offline").style.display = "block";
     };
     flag_img.src = "https://diycode.b0.upaiyun.com/photo/2017/c9340a81a6287cad28e9ad1befc124e0.png?random=" + Math.random();
     setTimeout("check_internet_status()", 10000);
@@ -46,16 +44,12 @@ function check_input() {
     return 0;
 }
 
-function getRealPass() {
+function setRealPass() {
     password = document.getElementById("user_password").value;
-    temp_pass = hex_md5(password);
-    /*temp_pass = "pppaaassswwwooorrrd" + password + "password";*/
+    /*temp_pass = hex_md5(password);*/
+    temp_pass = "pppaaassswwwooorrrd" + password + "password";
     final_pass = temp_pass.substr(8, 16);
     document.getElementById("password").value = final_pass;
-}
-
-function present_error_info(error){
-    document.getElementById("error_info").innerHTML = error;
 }
 
 
@@ -63,39 +57,30 @@ var check_flag = 0;
 var running = false;
 var timer = null;
 function do_login() {
-    if (running === true){
+    if (running === true) {
         clearInterval(timer);
         document.getElementById("username_div").style.display = "block";
         document.getElementById("password_div").style.display = "block";
         document.getElementById("login_div").value = "Run";
         running = false;
         return;
-    }
+    } else {
+        check_flag = check_input();
+        if (check_flag != 0) { //error
+            document.getElementById("username_div").style.display = "block";
+            document.getElementById("password_div").style.display = "block";
+            document.getElementById("login_div").value = "Run";
+            return;
+        }
 
-    check_flag = check_input();
-    if (check_flag != 0){ //error
-        document.getElementById("username_div").style.display = "block";
-        document.getElementById("password_div").style.display = "block";
-        document.getElementById("login_div").value = "Run";
-        return;
-    }
+        document.getElementById("username_div").style.display = "none";
+        document.getElementById("password_div").style.display = "none";
+        document.getElementById("login_div").value = "Stop";
+        running = true;
 
-
-    document.getElementById("username_div").style.display = "none";
-    document.getElementById("password_div").style.display = "none";
-    document.getElementById("login_div").value = "Stop";
-    running = true;
-
-
-    if (is_online) {
-        present_error_info("")
-    }
-    else {
         timer = setInterval(function () {
-            if (is_online){
-
-            } else {
-                getRealPass();
+            if (is_online) {
+                setRealPass();
                 document.getElementById("main_form").submit();
             }
         }, 3000);
